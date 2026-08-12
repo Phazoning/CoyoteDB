f3cdc7d4ea34e8a055f92512b8ae13003ca87005
phazoning <elgurudelasnecropolis@gmail.com>
2026-08-12 23:06:02 +0200

    tentative fix to buffer length not being appended correctly

--- src/tpm/command_functions.rs
+++ src/tpm/command_functions.rs
@@ -3,16 +3,30 @@ use super::definitions::{TPM_NO_SESSIONS_HEADER, TPM_CC_GETRANDOMNUMBER};
 use zerocopy::byteorder::big_endian::{U16, U32, U64, U128};
 
 pub fn generate_random_number_command() -> Buffer {
-    let mut command = Buffer::new();
-
-    command.add_u16(&U16::new(TPM_NO_SESSIONS_HEADER));
+    
 
     let mut body = Buffer::new();
 
     body.add_u32(&U32::new(TPM_CC_GETRANDOMNUMBER));
     body.add_u16(&U16::new(16 as u16));
 
-    command.add_tpmb2(body.get_self_buffer());
+    
+    let cmd = generate_command_no_sessions(body);
+    return cmd;
+}
 
-    return command;
+fn generate_command_no_sessions(body: Buffer) -> Buffer {
+    let mut command = Buffer::new();
+
+    command.add_u16(&U16::new(TPM_NO_SESSIONS_HEADER));
+
+    let mut length = body.get_length() as u32;
+
+    length += 6;
+
+    command.add_u32(&U32::new(length));
+
+    command.add_buffer(body.get_self_buffer());
+
+    return command
 }
