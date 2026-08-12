7d9fd438dfcd02d42e81ee66ebab1a03b3912d04
phazoning <elgurudelasnecropolis@gmail.com>
2026-08-12 21:47:14 +0200

    minor correction on one log requiring the Debug trait

--- src/tpm/win_core.rs
+++ src/tpm/win_core.rs
@@ -11,7 +11,7 @@ struct WindowsTPM{
 impl WindowsTPM{
     fn execute_command(&mut self, command: Buffer) -> Result<Buffer, CustomError> {
         println!("Executing command");
-        println!("Command body: {:?}", &command);
+        println!("Command body: {:?}", &command.get_self_buffer());
         let mut response = vec![0u8; 4096];
         let mut response_size = response.len() as u32;
 
